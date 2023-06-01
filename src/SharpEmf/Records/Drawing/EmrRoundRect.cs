af4d8818eb5145e32aee442daaea640ba7d4b4a5
Sergiusz Zalewski <38229504+KeterSCP@users.noreply.github.com>
2023-06-01 22:18:02 +0200

    #35 Implement parsing of EMR_ROUNDRECT

---
+++ src/SharpEmf/Records/Drawing/EmrRoundRect.cs
@@ -0,0 +1,39 @@
+﻿using JetBrains.Annotations;
+using SharpEmf.Enums;
+using SharpEmf.Interfaces;
+using SharpEmf.WmfTypes;
+
+namespace SharpEmf.Records.Drawing;
+
+/// <inheritdoc cref="EmfRecordType.EMR_ROUNDRECT"/>
+[PublicAPI]
+public record EmrRoundRect : EnhancedMetafileRecord, IEmfParsable<EmrRoundRect>
+{
+    public override EmfRecordType Type => EmfRecordType.EMR_ROUNDRECT;
+    public override uint Size { get; }
+
+    /// <summary>
+    /// Specifies the inclusive-inclusive bounding rectangle in logical units
+    /// </summary>
+    public RectL Box { get; }
+
+    /// <summary>
+    /// Specifies the width and height, in logical coordinates, of the ellipse used to draw the rounded corners
+    /// </summary>
+    public PointL Corner { get; }
+
+    private EmrRoundRect(uint size, RectL box, PointL corner)
+    {
+        Size = size;
+        Box = box;
+        Corner = corner;
+    }
+
+    public static EmrRoundRect Parse(Stream stream, uint size)
+    {
+        var box = RectL.Parse(stream);
+        var corner = PointL.Parse(stream);
+
+        return new EmrRoundRect(size, box, corner);
+    }
+}
